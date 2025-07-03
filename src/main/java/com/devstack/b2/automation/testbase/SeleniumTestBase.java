dd2c4b6520c42e000c0e28ccc482f5fe9ed32761
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-07-03 18:55:55 +0530

    SeleniumTestBase.java updated.

--- src/main/java/com/devstack/b2/automation/testbase/SeleniumTestBase.java
+++ src/main/java/com/devstack/b2/automation/testbase/SeleniumTestBase.java
@@ -1,5 +1,6 @@
 package com.devstack.b2.automation.testbase;
 
+import com.devstack.b2.automation.reporter.ExtentReportManager;
 import com.devstack.b2.automation.util.ThreadLocalWebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,14 +18,26 @@ public class SeleniumTestBase {
     }
 
     public void click(By locator) {
-        WebElement webElement = waitForVisibilityOfElement(locator, 10);
-        webElement.click();
+        try{
+            WebElement webElement = waitForVisibilityOfElement(locator, 10);
+            webElement.click();
+            ExtentReportManager.logPass("Clicked in locator : " + locator);
+        } catch (Exception e) {
+            ExtentReportManager.logFail("No such element: " + locator,"");
+        }
+
     }
     public void type(By locator, String inputText){
-        WebElement webElement = waitForVisibilityOfElement(locator, 10);
-        webElement.click();
-        webElement.clear();
-        webElement.sendKeys(inputText);
+        try {
+            WebElement webElement = waitForVisibilityOfElement(locator, 10);
+            webElement.click();
+            webElement.clear();
+            webElement.sendKeys(inputText);
+            ExtentReportManager.logPass("Typed [ "+inputText+" ] in locator : " + locator);
+        }catch (Exception e){
+            ExtentReportManager.logFail("No such element: " + locator,"");
+        }
+
     }
 
     public static WebElement waitForVisibilityOfElement(By locator,long timeout) {
