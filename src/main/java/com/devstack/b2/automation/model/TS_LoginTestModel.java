efc741601f619b36182e45da4e721eb2f1ff4a5e
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-07-08 19:36:54 +0530

    ExcelHandler.java , pom.xml updated. TS_LoginTestModel.java added.

---
+++ src/main/java/com/devstack/b2/automation/model/TS_LoginTestModel.java
@@ -0,0 +1,15 @@
+package com.devstack.b2.automation.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TS_LoginTestModel {
+    private String userName;
+    private String password;
+}
--- src/main/java/com/devstack/b2/automation/util/ExcelHandler.java
+++ src/main/java/com/devstack/b2/automation/util/ExcelHandler.java
@@ -1,11 +1,28 @@
 package com.devstack.b2.automation.util;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class ExcelHandler {
 
     private static final String EXCEL_PATH= "src/test/resources/excelData";
 
+    @DataProvider(name = "commonDataProvider")
     public static Object[][] commonDataProvider(Method method){
         String testClassName = method.getDeclaringClass().getSimpleName();
         String testMethodName = method.getName();
@@ -14,6 +31,51 @@ public class ExcelHandler {
         Class<?> modelClass = getModelClass(testClassName);
 
         return readExcelData(EXCEL_PATH+excelFileName,testMethodName,modelClass);
+    }
+
+    private static Object[][] readExcelData(String filePath, String sheetName, Class<?> modelClass) {
+        List<Object> dataList = new ArrayList<>();
+        try(FileInputStream file = new FileInputStream(new File(filePath))){
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+            XSSFSheet sheet = workbook.getSheet(sheetName);
+            if(sheet == null){
+                throw new RuntimeException("Sheet "+sheetName+" is not found in "+filePath+".");
+            }
+            Iterator<Row> rowIterator = sheet.iterator();
+            Row headerRow = rowIterator.next();
+
+            List<String> headers = new ArrayList<>();
+
+            for(Cell cell : headerRow){
+                headers.add(cell.getStringCellValue().trim());
+            }
+
+            while(rowIterator.hasNext()){
+                Row row = rowIterator.next();
+                Object modelInstance = modelClass.getDeclaredConstructor().newInstance();
+
+                for (int i = 0; i < headers.size(); i++) {
+                    Cell cell = row.getCell(i, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+                    String cellValue = (cell.getCellType() == CellType.STRING) ?
+                            cell.getStringCellValue() : String.valueOf((int) cell.getNumericCellValue());
+
+                    Field field = modelClass.getDeclaredField(headers.get(i));
+                    field.setAccessible(true);
+                    field.set(modelInstance, cellValue);
+                }
+                dataList.add(modelInstance);
+            }
+
+        } catch (IOException | InvocationTargetException | InstantiationException | IllegalAccessException |
+                 NoSuchMethodException | NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+
+       Object[][] dataArray =  new Object[dataList.size()][1];
+        for (int i = 0; i < dataList.size(); i++) {
+            dataArray[i][0] = dataList.get(i);
+        }
+        return dataArray;
     }
 
     private static Class<?> getModelClass(String testClassName) {
