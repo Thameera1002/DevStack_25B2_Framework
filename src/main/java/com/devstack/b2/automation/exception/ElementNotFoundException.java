291e2ea199a0978a1f90528e79bfb7b3fd3586d4
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-07-17 19:54:12 +0530

    ElementNotFoundException.java added

---
+++ src/main/java/com/devstack/b2/automation/exception/ElementNotFoundException.java
@@ -0,0 +1,7 @@
+package com.devstack.b2.automation.exception;
+
+public class ElementNotFoundException extends AutomationException {
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+}
